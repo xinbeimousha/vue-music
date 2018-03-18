9c2343f64075ce5f61fc3fbab1c64ce92ca6af58
xinbeimousha <m18675931249@163.com>
2018-03-18 13:16:19 +0800

    完成头部组件

---
+++ src/components/m-header/m-header.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="m-header">
+      <div class="icon"></div>
+      <h1 class="text">Chicken Music</h1>
+  </div>
+</template>
+<script>
+export default {
+  
+}
+</script>
+<style scoped lang="stylus">
+  @import "~common/stylus/variable"
+  @import "~common/stylus/mixin"
+  .m-header
+    position :relative
+    height:44px
+    text-align :center
+    color:$color-theme
+    font-size:0
+    .icon
+        display :inline-block
+        width:30px
+        height 32px;
+        vertical-align middle;
+        bg-image('logo')
+        background-size:30px 32px
+        margin-right :9px
+    .text
+      display: inline-block
+      vertical-align: middle
+      line-height: 44px
+      font-size: $font-size-large    
+</style>
+
+
